051cb15708a0fa135f1aab4d3e5ae1c89fa41830
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-11-12 18:19:08 +0200

    pages done

---
+++ front-end/public/search/scrypt.js
@@ -0,0 +1,24 @@
+function updateInformation() {
+    // Add logic for updating information
+    console.log("Updating Information");
+}
+
+function addProject() {
+    // Add logic for adding a project
+    console.log("Adding Project");
+}
+
+function deleteProject() {
+    // Add logic for deleting a project
+    console.log("Deleting Project");
+}
+
+function updateProject() {
+    // Add logic for updating a project
+    console.log("Updating Project");
+}
+
+function performSearch() {
+    // Add logic for performing search
+    console.log("Performing Search");
+}
