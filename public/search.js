541b865183cc0e6f423f3a3aab81bfc32549caf2
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-11-13 20:14:22 +0200

    search done

--- public/search.js
+++ public/search.js
@@ -1,29 +1,125 @@
-function updateInformation() {
-  // Add logic for updating information
-  console.log("Updating Information");
-}
-
-function addProject() {
-  // Add logic for adding a project
-  console.log("Adding Project");
-}
-
-function deleteProject() {
-  // Add logic for deleting a project
-  console.log("Deleting Project");
-}
-
-function updateProject() {
-  // Add logic for updating a project
-  console.log("Updating Project");
-}
-
-function performSearch() {
-  // Add logic for performing search
-  console.log("Performing Search");
-}
-
 const resultsDiv = document.querySelector(".search-results");
+const searchType = document.getElementById("searchType");
+const searchInput = document.getElementById("searchInput");
+const data = [
+  {
+    organization_id: 1,
+    title: "Education Initiative",
+    description:
+      "Providing educational resources for underprivileged children.",
+    location: "CityA",
+  },
+  {
+    id: 1,
+    username: "org1",
+    full_name: "Environmental Advocates",
+    email: "environment@example.com",
+    contact_telephone: "444-555-6666",
+    areas_of_interest: ["Environmental Conservation", "Wildlife Protection"],
+  },
+  {
+    id: 1,
+    username: "user2",
+    full_name: "John Doe",
+    email: "john@example.com",
+    contact_telephone: "123-456-7890",
+    areas_of_interest: ["Education", "Community Service"],
+    available_locations: ["CityA", "CityB"],
+  },
+  {
+    organization_id: 2,
+    title: "Community Outreach",
+    description: "Engaging with local communities to address their needs.",
+    location: "CityB",
+  },
+  {
+    id: 2,
+    username: "org2",
+    full_name: "Health for All",
+    email: "healthforall@example.com",
+    contact_telephone: "111-222-3333",
+    areas_of_interest: ["Healthcare", "Community Wellness"],
+  },
+  {
+    id: 2,
+    username: "user3",
+    full_name: "Alice Johnson",
+    email: "alice@example.com",
+    contact_telephone: "555-123-4567",
+    areas_of_interest: ["Technology", "Youth Development"],
+    available_locations: ["CityC", "CityD"],
+  },
+  {
+    organization_id: 3,
+    title: "Empowerment Program",
+    description: "Empowering individuals through skill development programs.",
+    location: "CityC",
+  },
+  {
+    organization_id: 4,
+    title: "Green Spaces Project",
+    description:
+      "Creating and maintaining green spaces for a healthier environment.",
+    location: "CityD",
+  },
+  {
+    organization_id: 5,
+    title: "Digital Literacy Campaign",
+    description: "Promoting digital literacy in underserved communities.",
+    location: "CityE",
+  },
+  {
+    id: 3,
+    username: "org3",
+    full_name: "Animal Guardians",
+    email: "animalguardians@example.com",
+    contact_telephone: "777-888-9999",
+    areas_of_interest: ["Animal Welfare", "Pet Adoption"],
+  },
+  {
+    id: 4,
+    username: "org4",
+    full_name: "Community Health Initiatives",
+    email: "healthinitiatives@example.com",
+    contact_telephone: "333-444-5555",
+    areas_of_interest: ["Public Health", "Health Education"],
+  },
+  {
+    id: 5,
+    username: "org5",
+    full_name: "Education for All",
+    email: "educationforall@example.com",
+    contact_telephone: "999-000-1111",
+    areas_of_interest: ["Education Access", "Literacy Programs"],
+  },
+  {
+    id: 3,
+    username: "user4",
+    full_name: "Bob Smith",
+    email: "bob@example.com",
+    contact_telephone: "888-777-6666",
+    areas_of_interest: ["Community Service", "Technology"],
+    available_locations: ["CityF", "CityG"],
+  },
+  {
+    id: 4,
+    username: "user5",
+    full_name: "Eva Martinez",
+    email: "eva@example.com",
+    contact_telephone: "666-555-4444",
+    areas_of_interest: ["Environment", "Youth Development"],
+    available_locations: ["CityH", "CityI"],
+  },
+  {
+    id: 5,
+    username: "user6",
+    full_name: "Charlie Brown",
+    email: "charlie@example.com",
+    contact_telephone: "444-333-2222",
+    areas_of_interest: ["Technology", "Community Wellness"],
+    available_locations: ["CityJ", "CityK"],
+  },
+];
 
 function appendBoxes(dataArray) {
   dataArray.forEach((obj) => {
@@ -55,47 +151,45 @@ function appendBoxes(dataArray) {
   });
 }
 
-const data = [
-  {
-    organization_id: 1,
-    title: "Education Initiative",
-    description:
-      "Providing educational resources for underprivileged children.",
-    location: "CityA",
-  },
-  {
-    id: 1,
-    username: "org1",
-    full_name: "Environmental Advocates",
-    email: "environment@example.com",
-    contact_telephone: "444-555-6666",
-    areas_of_interest: '{"Environmental Conservation","Wildlife Protection"}',
-  },
-  {
-    id: 1,
-    username: "user2",
-    full_name: "John Doe",
-    email: "john@example.com",
-    contact_telephone: "123-456-7890",
-    areas_of_interest: '{"Education","Community Service"}',
-    available_locations: '{"CityA","CityB"}',
-  },
-];
-
-const searchType = document.getElementById("searchType");
-const searchInput = document.getElementById("searchInput");
-
-function filterType() {
-    list = resultsDiv.children
+function filterInput() {
+  const list = resultsDiv.children;
   for (let i = 0; i < list.length; i++) {
-    if (list[i].classList.contains("correct")) {list[i].classList.remove("correct")}
-    if (list[i].classList.contains(`${searchType.value}`)) {list[i].classList += " correct"};
+    if (list[i].classList.contains("correct")) {
+      list[i].classList.remove("correct");
+    }
+    if (
+      list[i].innerHTML
+        .toLowerCase()
+        .includes(`${searchInput.value.toLowerCase()}`) &&
+      list[i].classList.contains(`${searchType.value}`)
+    ) {
+      list[i].classList += " correct";
+    }
   }
+}
+
+function updateInformation() {
+  // Add logic for updating information
+  console.log("Updating Information");
+}
+
+function addProject() {
+  // Add logic for adding a project
+  console.log("Adding Project");
+}
+
+function deleteProject() {
+  // Add logic for deleting a project
+  console.log("Deleting Project");
+}
+
+function updateProject() {
+  // Add logic for updating a project
+  console.log("Updating Project");
 }
 
 appendBoxes(data);
 
-filterType();
-searchType.addEventListener("input", filterType);
-
-console.log(resultsDiv.children[0]);
+filterInput();
+searchType.addEventListener("input", filterInput);
+searchInput.addEventListener("input", filterInput);
