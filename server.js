7cf82ac5e55cb4fe757d47aee0051bdd14914ed8
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-11-13 11:20:30 +0200

    forgot to branchout

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express()
-// const postsRoutes = require("./routes/...");
+const routes = require("./src/routes/index");
 const PORT = 5000;
 
 
 app.use(express.static("./public"))
 app.use(express.json());
 
-// app.use("/api/users", postsRoutes);
+app.use("/", routes);
 
 app.listen(PORT, () => console.log(`Port:${PORT}`));
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+// const controllerUsers = require("../controller/controller.users");
+// const controllerOrgs = require("../controller/controller.orgs");
+// const controllerProjects = require("../controller/controller.projects");
+
+router.get("/users/", controllerUsers.getAllUsers);
+router.get("/users/:id", controllerUsers.getUserById);
+router.post("/users/register", controllerUsers.registerUser);
+router.post("/users/login", controllerUsers.loginUser);
+router.put("/users/:id", controllerUsers.updateUser);
+
+router.get("/orgs/", controllerOrgs.getAllOrgs);
+router.get("/orgs/:id", controllerOrgs.getOrgById);
+router.post("/orgs/register", controllerOrgs.registerOrg);
+router.post("/orgs/login", controllerOrgs.loginOrg);
+router.put("/orgs/:id", controllerOrgs.updateOrg);
+
+router.get("/projects/", controllerProjects.getAllProjects);
+router.get("/projects/:id", controllerProjects.getProjectById);
+router.post("/projects/register", controllerProjects.registerProject);
+router.post("/projects/login", controllerProjects.loginProject);
+router.put("/projects/:id", controllerProjects.updateProject);
+
+module.exports = router;
